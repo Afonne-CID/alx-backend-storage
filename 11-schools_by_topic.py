ceb269878bae7ebe0f5bee866924adb7fa1f8c75
Afonne-CID <delightinbusiness@gmail.com>
2022-04-26 19:30:09 -0700

    filter MongoDB document in python

---
+++ 11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Task 11 module
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of schools having a specific topic
+    '''
+    return [i for i in mongo_collection.find(
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    )]
