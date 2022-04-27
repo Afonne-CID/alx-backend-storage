24a793645740f1c3e8057d435d0cd8df81af269b
Afonne-CID <delightinbusiness@gmail.com>
2022-04-26 19:40:36 -0700

    tring fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,10 +6,10 @@
 def schools_by_topic(mongo_collection, topic):
     '''Returns the list of schools having a specific topic
     '''
-    return [i for i in mongo_collection.find(
+    return [i for i in mongo_collection.find({
         'topics': {
             '$elemMatch': {
                 '$eq': topic,
             },
         },
-    )]
+    })]
